20a91f4d9634391d4ff5dbe5ffa10f13e565ee50
Naveenkumar0012 <naveenkumar.sightspectrum@gmail.com>
2023-09-13 17:35:04 +0530

    first commit

---
+++ loop/main.py
@@ -0,0 +1,60 @@
+# num1=int(input("enter the value:"))
+# num2=int(input("enter the value:"))
+# num3=int(input("enter the value:"))
+# num4=int(input("enter the value:"))
+# c = input("enter your choice:")
+# add = num1+num2+num3+num4
+# sub = num1-num2-num3-num4
+# multiply = num1*num2*num3*num4
+# divide = num1/num2/num3/num4
+# if c == 'add':
+#     print(add)
+# elif c == sub:
+#     print('sub')
+# elif c == 'multiply':
+#     print(multiply)
+# elif c == 'divide':
+##else:
+# n=1
+# for num in range(1,n+1):
+#    print(num)
+# num=1
+# breakpoint()
+# while True:
+#     print(num)
+#
+#     num +=1\
+# a=("567","46","48","42")
+# even_number = a
+# for even in a:
+#     even = int(even)
+#     if (even % == 0):
+#         print("the tupple value is:",[even])
+
+
+# var = {"name":["a","b","c","d"],"age":(33,22,56,17),"id":[7676,8989,89754,1132]}
+# c = var.get("age")
+# print(c)
+# even_number = c
+# for even in c:
+#     even = int(even)
+#     if even % 2 == 0:
+#         print("the even number in 'age':",(even))
+list=[1,2,3,4,5]
+for item in list[::3]:
+    print(item)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
